fcd425317e759c86aab9c9ffa8942e9936edb9b9
Adriana Amelia Troncoso Guerrero <AdrianaTroncoso-CloudUp>
2021-04-14 17:17:38 -0500

    Clase 3: metodo deprecado par llamar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,17 +5,31 @@ import Paper from '@material-ui/core/Paper'
 //*Componentes
 import Navbar from '../components/Navbar'
 export default class Home extends Component {
+
+    constructor(){
+        super()
+
+        this.state ={
+            fecha: new Date().toDateString(),
+            usuario: 'Visitante'
+        }
+    }
+
+    cambiarNombre = (e) =>{
+        this.setState({
+            usuario: 'Ady'
+        })
+    }
+
     render() {
         return(
             <div>
                 <Navbar/>
                 <Paper>
+                    <h1>{this.state.fecha}</h1>
+                    <h1>{this.state.usuario}</h1>
                     <h1>Home</h1>
-                    {
-                    // Date().toLocaleDateString()
-                    
-                    new Date().toLocaleDateString()
-                }
+                    <button onClick={this.cambiarNombre}>Cambiar Nombre</button>
                 </Paper>
 
             </div>
